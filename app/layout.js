6dab495489ea7b62152e8a51a914cb71055f6531
najmiddin <turgunpolatov3gmail.com>
2023-12-08 12:58:19 +0500

    rasm yangilash

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Logo from "../public/zoor-obed.jpg"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-screen-2xl mx-auto p-4">
+          <div className="flex justify-center">
+            <Image src={Logo} alt="Image" width={200} height={100} />
+          </div>
           <Navbar />
           <div className="mt-8">{children}</div>
         </div>
